65334eece17cb2fd6cb87b3d7914f2aacf6df98e
younggg96 <yangguannggeng960123@gmail.com>
2019-06-26 17:49:42 -0400

    YummyProject Final Project

---
+++ YummyProject/app/src/main/java/com/example/yummyproject/ItemActivity1.java
@@ -0,0 +1,26 @@
+package com.example.yummyproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ItemActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item1);
+        Button share_button = findViewById(R.id.button_share);
+        share_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Share");
+                startActivity(Intent.createChooser(intent, "Share Food"));
+            }
+        });
+    }
+}
